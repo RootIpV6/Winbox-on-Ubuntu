88703eb0399e01ee86ab9fb2c015749aa535d799
Ali Rıza SAYDAN <39958391+RootIpV6@users.noreply.github.com>
2024-08-22 20:59:59 +0300

    Update winbox-to-debian.sh

--- winbox-to-debian.sh
+++ winbox-to-debian.sh
@@ -1,7 +1,8 @@
-sudo dpkg --add-architecture i386
+dpkg --add-architecture i386 && apt-get update
 
+# debian repo
+
+echo "deb http://repo.vitexsoftware.com $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/vitexsoftware.list
+sudo wget -O /etc/apt/trusted.gpg.d/vitexsoftware.gpg http://repo.vitexsoftware.com/keyring.gpg
 sudo apt update
-
-sudo apt install wine
-
- wine /path/to/winbox.exe
+sudo apt install winbox
