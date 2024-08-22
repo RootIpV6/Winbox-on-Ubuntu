3401f3a94620a2129857da78d796ff77fa77bb65
ipv6 <39958391+RootIpV6@users.noreply.github.com>
2024-08-22 17:26:54 +0300

    .

---
+++ winbox-to-debian.sh
@@ -0,0 +1,14 @@
+sudo yum install epel-release
+
+sudo yum install wine -y
+
+wget https://mt.lv/winbox64
+
+wine ~/winbox64
+
+#set as winbox shortcut optional
+
+sudo mv ~/winbox64 /usr/local/bin/winbox
+
+wine /usr/local/bin/winbox
+
